f4be2986b3809d749b3afa0691538fce0c192299
Dnzzk2 <1933226149@qq.com>
2025-10-18 20:31:28 +0800

    up

--- src/lib/feed.ts
+++ src/lib/feed.ts
@@ -213,6 +213,13 @@ export async function generateRSS20(): Promise<string> {
     <pubDate>${lastBuildDate}</pubDate>
     <lastBuildDate>${lastBuildDate}</lastBuildDate>
     <generator>Astro Litos Theme</generator>
+    <image>
+      <url>${siteUrl}/favicon-192x192.png</url>
+      <title>${escapeXml(title)}</title>
+      <link>${siteUrl}</link>
+      <width>192</width>
+      <height>192</height>
+    </image>
     <atom:link href="${siteUrl}/rss.xml" rel="self" type="application/rss+xml" />
     ${processedPosts
       .map(
@@ -243,6 +250,13 @@ export async function generateAtom10(): Promise<string> {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="/rss/atom-styles.xsl"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
+  <image>
+    <url>${siteUrl}/favicon-192x192.png</url>
+    <title>${escapeXml(title)}</title>
+    <link>${siteUrl}</link>
+    <width>192</width>
+    <height>192</height>
+  </image>
   <title>${escapeXml(title)}</title>
   <subtitle>${escapeXml(description)}</subtitle>
   <link href="${siteUrl}/atom.xml" rel="self" type="application/atom+xml"/>
